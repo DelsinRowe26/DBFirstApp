6782162f8bd8f09eea0f77510543521ef372d245
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2023-02-28 13:11:23 +0700

    Добавьте файлы проекта.

---
+++ DBFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using DBFirstApp;
+
+using (HelloappContext db = new HelloappContext())
+{
+	var users = db.Users.ToList();
+	Console.WriteLine("Список объектов");
+	foreach (User user in users)
+	{
+		Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+	}
+}
